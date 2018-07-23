338fd4240a3731df0ce4e6a2c8ef1e9dd936d164
krachzack <hello@phstadler.com>
2018-07-23 21:55:11 +0200

    When translating, reusing mesh if possible

--- src/transform.rs
+++ src/transform.rs
@@ -1,21 +1,16 @@
 use aitios_geom::Vector3;
-use aitios_scene::{DeinterleavedIndexedMeshBuf, Entity};
+use aitios_scene::Entity;
 use std::rc::Rc;
 
 pub fn translate(entity: &mut Entity, offset: Vector3<f32>) {
-    entity.mesh = Rc::new(DeinterleavedIndexedMeshBuf {
-        positions: offset_position_vector(entity.mesh.positions.clone(), offset),
-        normals: entity.mesh.normals.clone(),
-        texcoords: entity.mesh.texcoords.clone(),
-        indices: entity.mesh.indices.clone(),
-    });
+    // Clone mesh, if shared with other entities, otherwise edit in place.
+    offset_position_vector(&mut Rc::make_mut(&mut entity.mesh).positions, offset);
 }
 
-fn offset_position_vector(mut positions: Vec<f32>, offset: Vector3<f32>) -> Vec<f32> {
+fn offset_position_vector(positions: &mut [f32], offset: Vector3<f32>) {
     positions.chunks_mut(3).for_each(|pos| {
         pos[0] += offset.x;
         pos[1] += offset.y;
         pos[2] += offset.z;
     });
-    positions
 }
